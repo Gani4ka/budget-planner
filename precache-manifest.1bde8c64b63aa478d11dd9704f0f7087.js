6d11d67735ae19ae4cbba4b55c2b44f0d0c5ad84
Anya Efimova <gani4ka_n@ukr.net>
2019-08-25 13:12:13 +0300

    Updates

---
+++ precache-manifest.1bde8c64b63aa478d11dd9704f0f7087.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146a49a1b65ea677defa84395cfd6928",
+    "url": "/budget-planner/index.html"
+  },
+  {
+    "revision": "9e5a2bd4e8a222549805",
+    "url": "/budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "78ee84abe97751e0a098",
+    "url": "/budget-planner/static/js/2.e0604ec0.chunk.js"
+  },
+  {
+    "revision": "9e5a2bd4e8a222549805",
+    "url": "/budget-planner/static/js/main.5a8ee482.chunk.js"
+  },
+  {
+    "revision": "6363082918b0fc50d71e",
+    "url": "/budget-planner/static/js/runtime~main.8ec2c2e1.js"
+  }
+]);
